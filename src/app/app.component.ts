7dcc801e23fcc4601a730773fd1cfc9aa7509d30
aikene <ikene.aj@gmail.com>
2021-12-13 19:34:43 -0500

    test build with unit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "AJ's Angular App Final Project 2";
+  title = "AJ's Angular App Final Project with Tests";
 }
